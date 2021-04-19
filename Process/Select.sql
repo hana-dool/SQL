0c25654e4d04852420842297295d43eb1a69fd8d
hana-dool <gorany131@gmail.com>
2021-04-19 23:33:26 +0900

    SQL 첫 commit

---
+++ Process/Select.sql
@@ -0,0 +1,11 @@
+select * 
+from employees ; 
+
+SELECT employee_id, email 
+from employees ; 
+
+-- 중복되는값 제거하기 --
+SELECT employee_id , first_name, last_name 
+FROM employees 
+ORDER BY employee_id DESC ; 
+
