e7d975f6b9b21fc86073b5bbdce14670037fb9b0
hana-dool <gorany131@gmail.com>
2021-05-20 02:57:07 +0900

    Update 서브퀄.sql

--- Process/서브퀄.sql
+++ Process/서브퀄.sql
@@ -54,12 +54,39 @@ FROM departments ;
 -- manager_id : 200
 -- location_id : 1700 을 삽입하기
 INSERT INTO departments (department_id,department_name,manager_id,location_id)
-VALUES (272,'GG',200,1700) ;   
+VALUES (280,'GG',200,1700) ;   
                   
 SELECT * 
 FROM departments ;
 
 -- department id 가 271 ~ 273 인 값들을 모두 삭제하자.
-
 DELETE FROM departments 
-WHERE department_id
+WHERE department_id BETWEEN 271 and 273 ; 
+
+
+-- department 테이블에 department_name 이 GG 인 행을 찾자.
+-- 그 이후에 manager_id 를 201, location_id 를 1800 으로 변경
+
+UPDATE departments
+SET manager_id = 201,
+    location_id = 1800
+WHERE department_name = 'GG' ;
+
+SELECT * 
+FROM departments 
+WHERE department_name = 'GG' ; 
+
+SELECT department_name
+FROM departments ;
+
+-- department 테이블에서 department_id 가 40인 manager_id 와 location_id 의 데이터값을 찾아내고
+-- department_name 이 'GG' 인 행의 manager_id 와 location_id 를 찾아낸 데이터값으로 변경
+UPDATE departments
+SET (manager_id,location_id) = (
+        SELECT manager_id, location_id
+        FROM departments
+        WHERE department_id = 40) 
+WHERE department_name = 'GG' ;
+
+DELETE FROM departments
+WHERE department_name = 'GG' ;
